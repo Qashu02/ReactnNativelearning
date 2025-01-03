561573b6aecaeda18019118ef7f07d10520b822c
qashu02 <hafizqasim5566@gmail.com>
2025-01-03 23:11:15 +0500

    Encapsulating styles with custom component

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, Platform, TouchableHighlight, Alert, TouchableOpacity, View , Button, SafeAreaView} from 'react-native';
 import WelcomeScreen from './app/screen/WelcomeScreen';
-import ShopScreen from './app/screen/ShopScreen';
+import AppText from './app/components/AppText';
 
 export default function App() {
  
-  return <ShopScreen/>
+  return <>
+  
+  <SafeAreaView style={styles.container}>
+
+    <AppText>Hello</AppText>
+  
+  </SafeAreaView>
+  
+  </>
  
 }
 
 const styles = StyleSheet.create({
   container: {
-    
+    flex:1,
     backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  
   
   },
 });
