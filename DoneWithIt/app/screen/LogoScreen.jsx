22ddce0780c7da3a1fa12e6bd0f5e55368d9ce3c
qashu02 <hafizqasim5566@gmail.com>
2025-01-10 01:30:19 +0500

    Validation with yup

--- DoneWithIt/app/screen/LogoScreen.jsx
+++ DoneWithIt/app/screen/LogoScreen.jsx
@@ -4,7 +4,14 @@ import Screen from '../components/Screen';
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
 import { Formik } from 'formik';
+import * as Yup from 'yup'
+import AppText from '../components/AppText';
+import colors from '../config/colors';
 
+const validationSchema= Yup.object().shape({
+    email:Yup.string().required().email().label('Email'),
+    password: Yup.string().required().min(4).label('Password')
+})
 function LogoScreen(props) {
 
     return (
@@ -12,8 +19,9 @@ function LogoScreen(props) {
 
     <Image style={styles.logo} source={require('../assets/favicon.png')} />
     <Formik initialValues={{email:'', password:''}}
-    onSubmit={(values)=>console.log(values)}>
-       {({handleChange,handleSubmit})=>
+    onSubmit={(values)=>console.log(values)}
+    validationSchema={validationSchema}>
+       {({handleChange,handleSubmit,errors})=>
        <>
 <AppTextInput name={'email'}
 placeholder='EnterEmail'
@@ -21,7 +29,7 @@ autoCapitalize='none'
 onChangeText={handleChange("email")}
 autoCorrect={false}
 keyboardType= 'email-address'/>
-
+<AppText style={{color:'red'}}>{errors.email}</AppText>
 <AppTextInput name={'lock'}
 placeholder='Enter Pass'
 onChangeText={handleChange("password")}
@@ -29,7 +37,7 @@ autoCapitalize='none'
 autoCorrect={false}
 keyboardType= 'numeric'
 secureTextEntry/>
-       
+       <AppText style={{color:"red"}}>{errors.password}</AppText>
 <Screen>
 
 <AppButton title={'login'} onPress={handleSubmit}/>
