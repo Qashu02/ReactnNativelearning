f05762c5fa1d71e8a6b0863006cb94b937ec2be5
qashu02 <hafizqasim5566@gmail.com>
2025-01-12 00:27:29 +0500

    Camera Permission

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -4,13 +4,20 @@ import LogoScreen from './app/screen/LogoScreen';
 import Screen from './app/components/Screen';
 import MessagesScreen from './app/screen/MessagesScreen';
 import ListingEditScreen from './app/screen/ListingEditScreen';
+import { useEffect } from 'react';
+import * as ImagePicker from 'expo-image-picker'
 
 
-
+const RequirePermission= async ()=>{
+const {granted}=await ImagePicker.requestCameraPermissionsAsync()
+if(!granted) alert("Please To access system you have to ensure permission")
+}
 
 export default function App() {
   
-
+useEffect(()=>{
+RequirePermission()
+},[])
   return <>
 <Screen>
 
