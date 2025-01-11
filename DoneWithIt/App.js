0fd784c437a0629d37e1ed63a63ce6ddfdc5fcf3
qashu02 <hafizqasim5566@gmail.com>
2025-01-12 01:34:27 +0500

    picking image fromdevice

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -4,24 +4,36 @@ import LogoScreen from './app/screen/LogoScreen';
 import Screen from './app/components/Screen';
 import MessagesScreen from './app/screen/MessagesScreen';
 import ListingEditScreen from './app/screen/ListingEditScreen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as ImagePicker from 'expo-image-picker'
 
-
 const RequirePermission= async ()=>{
-const {granted}=await ImagePicker.requestCameraPermissionsAsync()
-if(!granted) alert("Please To access system you have to ensure permission")
-}
+  
+  const {granted}=await ImagePicker.requestCameraPermissionsAsync()
+  if(!granted) alert("Please To access system you have to ensure permission")
+  }
 
 export default function App() {
+  const [imageUri, setImageUri]=useState()
   
 useEffect(()=>{
 RequirePermission()
 },[])
+const  selectImage= async ()=>{
+  try {
+    const result= await ImagePicker.launchImageLibraryAsync()
+    if(!result.canceled) setImageUri(result.uri)
+  } catch (error) {
+    console.log("The error is ",error)
+  }
+}
   return <>
-<Screen>
+<Screen style={styles.container}>
 
-<ListingEditScreen/>
+<Button title='Select Image' onPress={selectImage}/>
+<Image source={ {uri:imageUri}}style={{width:200,
+  height:300
+}}/>
 </Screen>
   </>
  
