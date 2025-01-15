41c38e6a37f44473cf0cfe782f06e5ceb1c80c4b
qashu02 <hafizqasim5566@gmail.com>
2025-01-15 22:29:58 +0500

    stack navigation

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,27 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, Platform, TouchableHighlight, Alert, TouchableOpacity, View, Button, SafeAreaView, Switch } from 'react-native';
+import { Text } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 import Screen from './app/components/Screen';
-import { useEffect, useState } from 'react';
-import * as ImagePicker from 'expo-image-picker';
-import AppImage from './app/components/ImageInput';
-import ImageInputList from './app/components/ImageInputList';
-import ListingEditScreen from './app/screen/ListingEditScreen';
- 
+
+const Tweets = () => (
+  <Screen>
+
+    <Text>Tweets</Text>
+  </Screen>
+);
+
+const TweetsDetails = () => (
+  <Screen>
+
+    <Text>Tweets Detail</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Tweets" component={Tweets} />
+      <Stack.Screen name="TweetsDetails" component={TweetsDetails} />
+    </Stack.Navigator>
+  );
+};
+
 export default function App() {
-  return<>
-
-    <ListingEditScreen/>
-  
-  </>
-   
-  
-  
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
