12d7982886655864f61a46adb8c0c15b12ba88ec
qashu02 <hafizqasim5566@gmail.com>
2025-01-18 00:03:41 +0500

    Api Layer

--- DoneWithIt/app/api/client.js
+++ DoneWithIt/app/api/client.js
@@ -4,8 +4,4 @@ const apiClient=create({
     baseURL:'http://192.168.0.104:9000/api'
 })
 
-apiClient.get('/listing').then(response=>{
-    if(!response.ok){
-        response.problem
-    }
-})
+export default apiClient;
